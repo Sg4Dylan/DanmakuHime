0f827f13eeeef79ea32c2831736321a73636cba2
Sg4Dylan <dengbill123@gmail.com>
2016-01-22 23:03:44 +0800

    fixed application not responding

--- SendDanmaku/Send.cs
+++ SendDanmaku/Send.cs
@@ -83,20 +83,11 @@ namespace SendDanmaku
             byte[] byteArray = Encoding.UTF8.GetBytes(postData);
             request.ContentType = "application/x-www-form-urlencoded";
             request.CookieContainer = new CookieContainer();
-            request.CookieContainer.SetCookies(new Uri("http://live.bilibili.com/"),cookie);
+            request.CookieContainer.SetCookies(new Uri("http://live.bilibili.com/"), cookie);
             request.ContentLength = byteArray.Length;
             Stream dataStream = request.GetRequestStream();
             dataStream.Write(byteArray, 0, byteArray.Length);
             dataStream.Close();
-            WebResponse response = request.GetResponse();
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-            dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-            Console.WriteLine(responseFromServer);
-            reader.Close();
-            dataStream.Close();
-            response.Close();
             danmaku_inputbox.Text = null;
         }
 
